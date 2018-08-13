1fa65355f04c7711c7e66bc1fdd300ef9efac256
faith-goto <goto@faith.co.jp>
2018-08-13 02:22:48 +0000

    bootstrap導入

--- exapp/app/models/test_result.rb
+++ exapp/app/models/test_result.rb
@@ -1,4 +1,7 @@
 class TestResult < ApplicationRecord
   belongs_to :user
   belongs_to :what_test
+
+  validates :user_id,:what_test_id,:jpn ,:math ,:eng ,:sci ,:soc, presence: true
+  validates :jpn ,:math ,:eng ,:sci ,:soc, inclusion: {in: 0..100}
 end
--- exapp/config/routes.rb
+++ exapp/config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :users
   resources :what_tests
   resources :periods
+
+  get '/', to: 'test_results#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
